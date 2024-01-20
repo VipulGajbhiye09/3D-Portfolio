9bf410be064ccf9c048f62267547fda21db8caae
VipulGajbhiye09 <vipulgajbhiye099@gmail.com>
2024-01-20 19:30:56 +0530

    Navbar - Hamburger menu done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,8 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
-  // const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(true);
-
 
   return (
     <nav
@@ -48,8 +47,42 @@ const Navbar = () => {
           ))}
         </ul>
 
-        
+        {/* hamburger menu  */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          {/* hamburger image*/}
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
+            onClick={() => setToggle(!toggle)}
+          />
 
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            {/* hamburger links on toggle on */}
+            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+
+          </div>  
+        </div>
+        
       </div>
     </nav>
   );
